fb4825192d25c5c4e9a018119d67efae1754ee21
1afzal <afzalali5256@gmail.com>
2025-10-31 10:53:19 +0530

    added sora

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5173,
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
